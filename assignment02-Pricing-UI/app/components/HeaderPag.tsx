a424dbcde37d83b25ae638cb459a5d66962c96dc
Saud <saud@gmail.com>
2023-03-17 14:31:10 +0500

    next.js assigment

--- assignment02-Pricing-UI/app/components/HeaderPag.tsx
+++ assignment02-Pricing-UI/app/components/HeaderPag.tsx
@@ -1,10 +1,12 @@
 export default function header(){
     return (
 
-    <div>
-      <div className="flex flex-col items-center bg-violet-800 h-80 text-gray-200 flex-wrap">
-      <p className="text-3xl font-bold mt-40"> Simple pricing for your Saud Ahmed PIAIC188121 </p>
-      <p className="mt-2">Plains that are carefully crafted to suit your business.</p>
+    <div className="flex flex-col items-center bg-violet-600 h-96 text-gray-200 flex-wrap">
+      <div >
+      <p className="flex-row md:flex-col text-3xl font-bold mt-44 flex pl-4 text-gray-200 pr-2"> Simple pricing for your SAUD PIAIC188121</p>
+      </div>
+      <div>
+      <p className="mt-3 text-gray-200 pl-4 pr-2 flex flex-wrap">Plains that are carefully crafted to suit your business.</p>
       </div>
     </div>
 
--- assignment02-Pricing-UI/app/components/Pricing.tsx
+++ assignment02-Pricing-UI/app/components/Pricing.tsx
@@ -2,32 +2,32 @@ import CheckIconMark from "./CheckMarkIcons";
 
 export default function Pricing(){
     return(
-        <div className="flex justify-center drop-shadow-2xl flex-wrap">
-            <div className="bg-violet-200 p-16 flex flex-col items-center rounded-xl"> 
-            <p className="font-bold text-2xl">Premium PRO</p>
+        <div className=" max-w-full flex flex-col md:flex-row drop-shadow-xl justify-center mx-2">
+            <div className="bg-violet-200 p-12 flex flex-col items-center rounded-xl">
+            <p className="font-bold text-2xl mb-2">Premium PRO</p>
             <p className="font-bold text-7xl">$359</p>
-            <p>Billed just once</p>
-            <button className="bg-violet-700 w-80 rounded mt-6 py-3 text-gray-100 font-semibold text-md hover:bg-violet-600 active:bg-violet-500">Get Started</button>
+            <p className="text-gray-900 mt-1">Billed just once</p>
+            <button className=" bg-violet-700 w-80 rounded mt-10 py-3 text-gray-100 font-semibold text-md hover:bg-violet-600 active:bg-violet-500"><p>Get Started</p></button>
             </div>
-            <div className="bg-white pt-12 pl-4 pr-8 rounded-xl "> 
+            <div className="bg-white pt-12 pl-4 pr-2 rounded-xl "> 
             <p>Access these features when you get this pricing package for your business.</p>
             <div className="mt-4">
             <div>
                 <div className="flex">
                 <CheckIconMark/>
-                <p className="mb-2">International calling and messaging API</p>
+                <p className="mb-4">International calling and messaging API</p>
                 </div>
                 <div className="flex">
                 <CheckIconMark/>
-                <p className="mb-2">Additional phone numbers</p>
+                <p className="mb-4">Additional phone numbers</p>
                 </div>
                 <div className="flex">
                 <CheckIconMark/>
-                    <p className="mb-2">Automated messages via Zapier</p>
+                    <p className="mb-4">Automated messages via Zapier</p>
                 </div>
                 <div className="flex"> 
                 <CheckIconMark/>
-                <p className="mb-2">24/7 support and consulting</p>
+                <p className="mb-4">24/7 support and consulting</p>
                 </div>
                 
                 </div>
